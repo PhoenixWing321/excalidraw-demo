7321c273ad9602cee28d2449bc08b272ea028108
phoenix <3301647@qq.com>
2025-03-07 10:54:27 +0800

    添加：excalidraw

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import { useState } from 'react';
 
+import { Excalidraw } from "@excalidraw/excalidraw";
+
 function App() {
   const [count, setCount] = useState<number>(0);
 
   return (
-    <div className="container">
-      <h1>Vite + React + TypeScript</h1>
-      <button onClick={() => setCount(count + 1)}>
-        Count: {count}
-      </button>
+
+    <div style={{ height: "70vh", width: "90%" }}>
+      <Excalidraw theme="dark" />
+      <div className="container">
+        <h1>Vite + React + TypeScript + Excalidraw</h1>
+        <button onClick={() => setCount(count + 1)}>
+          Count: {count}
+        </button>
+      </div>
     </div>
   );
 }
